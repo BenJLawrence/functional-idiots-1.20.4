0345da0fc756acbc229816b6757ec14f39912062
RealDragonHours <ben.lawrence0324@gmail.com>
2024-08-09 23:23:19 -0500

    Initial commit

---
+++ src/main/java/draggle/idiots/FunctionalIdiots.java
@@ -0,0 +1,16 @@
+package draggle.idiots;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FunctionalIdiots implements ModInitializer {
+	public static final String MOD_ID = "functional-idiots";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/draggle/idiots/FunctionalIdiotsClient.java
@@ -0,0 +1,10 @@
+package draggle.idiots;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunctionalIdiotsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
